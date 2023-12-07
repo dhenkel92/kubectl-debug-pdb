f7bbfcea3cc609850311f504d8d6b06672658183
Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>
2023-12-07 10:29:06 +0100

    feature: merge evict pod command into debug-pdb (#9)

--- pkg/cmd/evict_pod.go
+++ pkg/cmd/evict_pod.go
@@ -37,7 +37,7 @@ func NewCmdEvictPod(streams genericclioptions.IOStreams, configFlags *genericcli
 	o := NewEvictPodOptions(streams, configFlags)
 
 	cmd := &cobra.Command{
-		Use:   "pod <pod_name>",
+		Use:   "evict <pod_name>",
 		Short: "Utility to evict a pod from a node",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if err := o.Complete(cmd, args); err != nil {
